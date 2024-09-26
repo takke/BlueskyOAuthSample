7d6c7f9cd95c1f223ceecc2a00880444dabd3e55
takke <takke30@gmail.com>
2024-09-26 22:56:10 +0900

    OAuthHelper.makeDpopHeader

--- app/src/main/java/com/example/blueskyoauthsample/MainActivity.kt
+++ app/src/main/java/com/example/blueskyoauthsample/MainActivity.kt
@@ -331,51 +331,7 @@ class MainActivity : ComponentActivity() {
         appendResult("----- ES256 Private Key -----")
         appendResult(String(privateKeyEncodedBytes))
 
-        // kotlinx-serialization で JSON を生成
-
-        // ヘッダー部生成
-        val headerJson = buildJsonObject {
-            put("alg", "ES256")
-            put("typ", "dpop+jwt")
-            put("jwk", buildJsonObject {
-                put("kty", "EC")
-                put("crv", "P-256")
-                put("x", Base64.encode(publicKey.w.affineX.toByteArray()))
-                put("y", Base64.encode(publicKey.w.affineY.toByteArray()))
-            })
-        }
-        appendResult("----- JWT Header -----")
-        val formatter = Json { prettyPrint = true }
-//        appendResult(headerJson.toString())
-        appendResult(formatter.encodeToString(headerJson))
-
-        // ペイロード部生成
-        val payloadJson = buildJsonObject {
-            // iss, ath は PDS へのリクエスト時に必須
-            put("iss", clientId)
-            put("sub", clientId)
-            put("htu", endpoint)
-            put("htm", method)
-            val epoch = System.currentTimeMillis() / 1000
-            put("exp", epoch + 60)
-            // random token string (unique per request)
-            put("jti", generateRandomValue())
-            put("iat", epoch)
-            put("nonce", dpopNonce)
-        }
-        appendResult("----- JWT Payload -----")
-//        appendResult(payloadJson.toString())
-        appendResult(formatter.encodeToString(payloadJson))
-
-        // 署名
-        val headerBase64 = Base64.encode(headerJson.toString().toByteArray())
-        val payloadBase64 = Base64.encode(payloadJson.toString().toByteArray())
-        val jwtMessage = "$headerBase64.$payloadBase64"
-
-        // 署名生成
-        // "SHA256withECDSAinP1363Format" は Android で使用不可
-//        val jwtSignature = Signature.getInstance("SHA256withECDSAinP1363Format")
-
+        // "SHA256withPLAIN-ECDSA" を使えるようにする
         val provider = Security.getProvider(BouncyCastleProvider.PROVIDER_NAME)
         if (provider.javaClass != BouncyCastleProvider::class.java) {
             // Android には BouncyCastle の短縮版が既に含まれていてそれには "SHA256withPLAIN-ECDSA" が含まれていないので
@@ -387,13 +343,29 @@ class MainActivity : ComponentActivity() {
         } else {
             appendResult("BouncyCastleProvider はすでに登録されています")
         }
-        val jwtSignature = Signature.getInstance("SHA256withPLAIN-ECDSA", "BC") //IEEE P1363
 
-        jwtSignature.initSign(privateKey)
-        jwtSignature.update(jwtMessage.toByteArray())
-        val jwtSignatureBytes: ByteArray = jwtSignature.sign()
-        val jwtSignatureStr: String = Base64.encode(jwtSignatureBytes)
-        val jwt = "$headerBase64.$payloadBase64.$jwtSignatureStr"
+        val jwt = OAuthHelper.makeDpopHeader(
+            clientId = clientId,
+            endpoint = endpoint,
+            method = method,
+            dpopNonce = dpopNonce,
+            publicKeyWAffineX = publicKey.w.affineX.toByteArray(),
+            publicKeyWAffineY = publicKey.w.affineY.toByteArray(),
+            sign = { jwtMessage ->
+                // jwtMessage を署名し、バイト列を返す
+
+                // "SHA256withECDSAinP1363Format" は Android で使用不可
+//              val jwtSignature = Signature.getInstance("SHA256withECDSAinP1363Format")
+
+                val jwtSignature = Signature.getInstance("SHA256withPLAIN-ECDSA", "BC") // IEEE P1363
+
+                // jwtMessage の署名
+                jwtSignature.initSign(privateKey)
+                jwtSignature.update(jwtMessage.toByteArray())
+                jwtSignature.sign()
+            }
+        )
+
         appendResult("----- JWT -----")
         appendResult(jwt)
 
@@ -419,12 +391,4 @@ class MainActivity : ComponentActivity() {
         return hashBytes.joinToString("") { "%02x".format(it) }
     }
 
-    private fun generateRandomValue(): String {
-        // ランダム値を生成
-        val randomValueBytes = ByteArray(32)
-        Random.nextBytes(randomValueBytes)
-
-        // BASE64エンコード
-        return randomValueBytes.joinToString("") { "%02x".format(it) }
-    }
 }
---
+++ app/src/main/java/com/example/blueskyoauthsample/OAuthHelper.kt
@@ -0,0 +1,82 @@
+package com.example.blueskyoauthsample
+
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.put
+import kotlin.io.encoding.Base64
+import kotlin.io.encoding.ExperimentalEncodingApi
+import kotlin.random.Random
+
+object OAuthHelper {
+
+    @OptIn(ExperimentalEncodingApi::class)
+    fun makeDpopHeader(
+        clientId: String,
+        endpoint: String,
+        method: String,
+        dpopNonce: String,
+        // 公開鍵の x 値、y 値
+        publicKeyWAffineX: ByteArray,
+        publicKeyWAffineY: ByteArray,
+        // 秘密鍵でメッセージを署名する lambda
+        sign: (String) -> ByteArray
+    ): String {
+
+        // kotlinx-serialization で JSON を生成
+
+        // ヘッダー部生成
+        val headerJson = buildJsonObject {
+            put("alg", "ES256")
+            put("typ", "dpop+jwt")
+            put("jwk", buildJsonObject {
+                put("kty", "EC")
+                put("crv", "P-256")
+                put("x", Base64.encode(publicKeyWAffineX))
+                put("y", Base64.encode(publicKeyWAffineY))
+            })
+
+        }
+//        appendResult("----- JWT Header -----")
+//        val formatter = Json { prettyPrint = true }
+////        appendResult(headerJson.toString())
+//        appendResult(formatter.encodeToString(headerJson))
+
+        // ペイロード部生成
+        val payloadJson = buildJsonObject {
+            // iss, ath は PDS へのリクエスト時に必須
+            put("iss", clientId)
+            put("sub", clientId)
+            put("htu", endpoint)
+            put("htm", method)
+            val epoch = System.currentTimeMillis() / 1000
+            put("exp", epoch + 60)
+            // random token string (unique per request)
+            put("jti", generateRandomValue())
+            put("iat", epoch)
+            put("nonce", dpopNonce)
+        }
+//        appendResult("----- JWT Payload -----")
+////        appendResult(payloadJson.toString())
+//        appendResult(formatter.encodeToString(payloadJson))
+
+        // 署名
+        val headerBase64 = Base64.encode(headerJson.toString().toByteArray())
+        val payloadBase64 = Base64.encode(payloadJson.toString().toByteArray())
+        val jwtMessage = "$headerBase64.$payloadBase64"
+
+        // 署名生成
+        val jwtSignatureBytes: ByteArray = sign(jwtMessage)
+        val jwtSignatureStr: String = Base64.encode(jwtSignatureBytes)
+        val jwt = "$headerBase64.$payloadBase64.$jwtSignatureStr"
+
+        return jwt
+    }
+
+    private fun generateRandomValue(): String {
+        // ランダム値を生成
+        val randomValueBytes = ByteArray(32)
+        Random.nextBytes(randomValueBytes)
+
+        // BASE64エンコード
+        return randomValueBytes.joinToString("") { "%02x".format(it) }
+    }
+}
